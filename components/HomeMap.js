8c35aefe2dd613cbed3eb00826023187bf400064
Nahom Amanuel <namanuel11@gmail.com>
2022-12-30 10:54:20 -0500

    Commented out flat list from home screen

--- components/HomeMap.js
+++ components/HomeMap.js
@@ -9,10 +9,10 @@ const Map = () => {
     latitude: 37.78825,
     longitude: -122.4324,
   });
-  
-  
+
+
   const [collapsed, setCollapsed] = useState(true);
-  
+
 
   useEffect(() => {
     (async () => {
@@ -28,83 +28,83 @@ const Map = () => {
 
   const data = [
     {
-      id: "ecoButton", 
+      id: "ecoButton",
       title: "Eco's",
       image: "https://i.postimg.cc/x1DPWSCp/blue-minimalist-network-logo-design.png",
       screen: "PersonalEcoScreen"
     },
     {
-      id: "createEco", 
+      id: "createEco",
       title: "Add Event",
       image: "https://i.postimg.cc/V6ChjWHd/blue-minimalist-network-logo-design-1.png",
       screen: "AddEventScreen"
-    }, 
+    },
     {
-        id: "todayEvents", 
-        title: "Events",
-        image: "https://i.postimg.cc/xjsmvVMN/blue-minimalist-network-logo-design-2.png",
-        screen: "todaysEventsScreen"
-      }
+      id: "todayEvents",
+      title: "Events",
+      image: "https://i.postimg.cc/xjsmvVMN/blue-minimalist-network-logo-design-2.png",
+      screen: "todaysEventsScreen"
+    }
   ];
-  
-  return (
-    <View style = {styles.container}>
-    <MapView
-      style={styles.map}
-      draggable
-      onPanDrag={() => setCollapsed(true)}
-      region={{
-        latitude: location.latitude,
-        longitude: location.longitude,
-        latitudeDelta: 0.009000009,
-        longitudeDelta: 0.0090000091,
-      }}
-    >
-      <Marker
-        coordinate={location}
-        title='My Location'
-        description='This is my current location'
-       
-      />
-    </MapView>
-    
-    <View style={styles.buttonContainer}>
-  <TouchableOpacity onPress={() => setCollapsed(!collapsed)} style={styles.button}>
-    
-      <View style = {{marginLeft: 10, backgroundColor: "#C0C0C0", borderRadius: 20, padding: 14}}>
-        
-        <Image className = "h-7 w-7 align-middle center"
-      source = {{uri: 'https://i.postimg.cc/dtCHxnh8/menu.png'}}
-      />
-      </View>
-    
-  </TouchableOpacity>
-    <FlatList
-    style = {{height: collapsed ? 0 : 'auto'}}
-    styles = {styles.list}
-    data = {data}
-    keyExtractor = {(item) => item.id}
-    vertical
-    renderItem = {({item}) => (
-    <TouchableOpacity style= {styles.item}>
-      <View className = "h-20 w-20 align-middle center pt-3 pl-2">
-        <Image
-        style = {{width: 30, height: 30, resizeMode: "contain", marginLeft: 20}}
-        source = {{uri: item.image}}
-        />
-        <Text className = {"text-center font-Arial"}>
-           
-           {item.title}
-        </Text>
-      </View>
-    </TouchableOpacity>)}
-    collapsable = {collapsed}
-      
 
-    />
-    </View>
-    
-    
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        draggable
+        onPanDrag={() => setCollapsed(true)}
+        region={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.009000009,
+          longitudeDelta: 0.0090000091,
+        }}
+      >
+        <Marker
+          coordinate={location}
+          title='My Location'
+          description='This is my current location'
+
+        />
+      </MapView>
+
+      {/* <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={() => setCollapsed(!collapsed)} style={styles.button}>
+
+          <View style={{ marginLeft: 10, backgroundColor: "#C0C0C0", borderRadius: 20, padding: 14 }}>
+
+            <Image className="h-7 w-7 align-middle center"
+              source={{ uri: 'https://i.postimg.cc/dtCHxnh8/menu.png' }}
+            />
+          </View>
+
+        </TouchableOpacity>
+        <FlatList
+          style={{ height: collapsed ? 0 : 'auto' }}
+          styles={styles.list}
+          data={data}
+          keyExtractor={(item) => item.id}
+          vertical
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.item}>
+              <View className="h-20 w-20 align-middle center pt-3 pl-2">
+                <Image
+                  style={{ width: 30, height: 30, resizeMode: "contain", marginLeft: 20 }}
+                  source={{ uri: item.image }}
+                />
+                <Text className={"text-center font-Arial"}>
+
+                  {item.title}
+                </Text>
+              </View>
+            </TouchableOpacity>)}
+          collapsable={collapsed}
+
+
+        />
+      </View> */}
+      {/* Collapsible Flat List */}
+
     </View>
   );
 }
@@ -116,7 +116,7 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
-    height: '100%', 
+    height: '100%',
   },
   buttonContainer: {
     position: 'absolute',
@@ -129,7 +129,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
   list: {
-  
+
     overflow: 'hidden',
     position: 'absolute',
     top: 0,
@@ -139,7 +139,7 @@ const styles = StyleSheet.create({
   },
   item: {
     backgroundColor: '#C0C0C0',
-    
+
     borderRadius: 30,
     marginTop: 10
   },
